abeac381c68dc09129c43e6b6720d18aa964f8dd
dhruv-08 <dgarg.cse18@chitkarauniversity.edu.in>
2020-09-05 22:42:37 +0530

    dialog transfer

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,12 +1,14 @@
-import { Button, TextField } from '@material-ui/core';
+import { Button, Dialog, DialogContent, DialogContentText, DialogTitle, TextField } from '@material-ui/core';
 import Axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form'
+import DoneIcon from '@material-ui/icons/Done';
 import "../CSS/fileUpload.css"
 function FileUpload() {
   const { register, handleSubmit } = useForm()
   const [filetransfer, setfiletransfer] = useState(null)
   const [usertext, setusertext] = useState('');
+  const [success, setsuccess] = useState(false);
   const [formval, setformval] = useState(null);
   const onSubmit = async () => {
     const formData = new FormData()
@@ -20,6 +22,10 @@ function FileUpload() {
        console.log(res);
      })
    })
+   setsuccess(true);
+   setTimeout(() => {
+       setsuccess(false);
+     }, 2000);
   }
   return (
     <div style={{position:"absolute",paddingTop:"10%"}}>
@@ -29,6 +35,18 @@ function FileUpload() {
       <TextField id="outlined-basic" label="Send To" variant="outlined" value={usertext} name="username" onChange={(e)=>setusertext(e.target.value)} style={{width:"400px"}}/><br/><br/>
       <Button style={{width:"400px"}} type="submit" variant="contained" color="primary">Transfer</Button>
     </form>
+    {success===true && <Dialog
+                    style={{color:"black"}}
+                    open={true}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description">
+                    <DialogTitle id="alert-dialog-title" style={{backgroundColor:"black"}}><span style={{color:"white"}}>Transfer Successfully</span></DialogTitle>
+                    <DialogContent style={{backgroundColor:"black",textAlign:"center"}}>
+                    <DialogContentText id="alert-dialog-description">
+                    <DoneIcon style={{color:"white"}}/>
+                    </DialogContentText>
+                    </DialogContent>
+                </Dialog>}
     </div>
   );
 }
